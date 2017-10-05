49659da2eb2964cc59dea36573d4d12c129db1da
riteshghorse <riteshghorse@gmail.com>
2017-10-05 12:38:40 +0530

    dabba

--- KitchAs/kitchen/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class KitasConfig(AppConfig):
-    name = 'kitchen'
--- KitchAs/kitchen/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class PostForm(forms.Form):
-		email = forms.CharField(max_length=256)
-		password = forms.CharField(max_length=256)
